985786abe271b1c3afefdc9aecc8efd6cebc718f
NPinzin <nick.pinzin@gmail.com>
2025-10-14 13:57:51 -0400

    Initial provider info

---
+++ project.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = 
+  secret_key = 
+}
+
+resource "aws_instance" "ec2example" {
+    ami = 
+    instance_type = "t2.micro"
+    security_groups = []
+}
